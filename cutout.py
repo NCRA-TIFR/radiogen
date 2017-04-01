58cb3b8cc1c7371b8453cffb1f94e983934821d1
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2017-04-01 11:43:33 +0530

    Change input filename in cutout source file

--- cutout.py
+++ cutout.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 CUTOUT_SIZE = 10
 #Assuming PyBDSM has already been run to generate source catalog
 header = "Source_id, Isl_id, RA, E_RA, DEC, E_DEC, Total_flux, E_Total_flux, Peak_flux, E_Peak_flux, RA_max, E_RA_max, DEC_max, E_DEC_max, Maj, E_Maj, Min, E_Min, PA, E_PA, Maj_img_plane, E_Maj_img_plane, Min_img_plane, E_Min_img_plane, PA_img_plane, E_PA_img_plane, DC_Maj, E_DC_Maj, DC_Min, E_DC_Min, DC_PA, E_DC_PA, DC_Maj_img_plane, E_DC_Maj_img_plane, DC_Min_img_plane, E_DC_Min_img_plane, DC_PA_img_plane, E_DC_PA_img_plane, Isl_Total_flux, E_Isl_Total_flux, Isl_rms, Isl_mean, Resid_Isl_rms, Resid_Isl_mean, S_Code".split(', ')
-catalog = pd.read_csv('TGSSADR_R05D45_5x5.MOSAIC.pybdsm.srl', skiprows=6, names=header)
-data = fits.open('TGSSADR_R05D45_5x5.MOSAIC.pybdsm_gaus_model.fits')[0].data[0][0]
-w = wcs.WCS('TGSSADR_R05D45_5x5.MOSAIC.pybdsm_gaus_model.fits')
+
+catalog = pd.read_csv('R05D45_5x5.MOSAIC.pybdsm.srl', skiprows=6, names=header)
+data = fits.open('R05D45_5x5.MOSAIC.pybdsm_gaus_model.fits')[0].data[0][0]
+w = wcs.WCS('R05D45_5x5.MOSAIC.pybdsm_gaus_model.fits')
 
 for i in range(len(catalog)):
     RA = catalog.iloc[i]['RA']
